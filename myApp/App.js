e6753337d1ca4df074bb430be6e43e89689be1d8
rubenofen <rubenofen@gmail.com>
2025-06-02 19:35:27 +0100

    Scroll view

--- myApp/App.js
+++ myApp/App.js
@@ -1,12 +1,10 @@
-import { useRef, useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import { Button } from "./components/Button";
 
 export default function App() {
-  const [count, setCount] = useState(0);
   const [name, setName] = useState("");
   const [surname, setSurname] = useState("");
-  const nameRef = useRef(null);
   const onSubmit = () => {
     console.log("Form submitted with:", {
       name,
@@ -15,7 +13,7 @@ export default function App() {
     // Here you can handle the form submission, e.g., send data to an API
   };
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={styles.form}>
         <Text style={styles.title}>Formulario</Text>
         <TextInput
@@ -32,8 +30,71 @@ export default function App() {
         />
         <Button title="Save" onPress={onSubmit} color="blue" />
       </View>
-      <Text style={styles.counter}>Contador {count}</Text>
-    </View>
+      <View style={styles.form}>
+        <Text style={styles.title}>Formulario</Text>
+        <TextInput
+          style={styles.input}
+          value={name}
+          placeholder="Escribe algo"
+          onChangeText={setName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Escribe algo"
+          value={surname}
+          onChangeText={setSurname}
+        />
+        <Button title="Save" onPress={onSubmit} color="blue" />
+      </View>
+      <View style={styles.form}>
+        <Text style={styles.title}>Formulario</Text>
+        <TextInput
+          style={styles.input}
+          value={name}
+          placeholder="Escribe algo"
+          onChangeText={setName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Escribe algo"
+          value={surname}
+          onChangeText={setSurname}
+        />
+        <Button title="Save" onPress={onSubmit} color="blue" />
+      </View>
+      <View style={styles.form}>
+        <Text style={styles.title}>Formulario</Text>
+        <TextInput
+          style={styles.input}
+          value={name}
+          placeholder="Escribe algo"
+          onChangeText={setName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Escribe algo"
+          value={surname}
+          onChangeText={setSurname}
+        />
+        <Button title="Save" onPress={onSubmit} color="blue" />
+      </View>
+      <View style={styles.form}>
+        <Text style={styles.title}>Formulario</Text>
+        <TextInput
+          style={styles.input}
+          value={name}
+          placeholder="Escribe algo"
+          onChangeText={setName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Escribe algo"
+          value={surname}
+          onChangeText={setSurname}
+        />
+        <Button title="Save" onPress={onSubmit} color="blue" />
+      </View>
+    </ScrollView>
   );
 }
 
@@ -41,8 +102,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
   title: {
     fontSize: 20,
@@ -66,7 +125,6 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   form: {
-    flex: 1,
     justifyContent: "center",
     gap: 10,
     backgroundColor: "lightgray",
